6a9aba299428f1bd14700879f58c79b77a29ad6e
deepak22s <130236776+deepak22s@users.noreply.github.com>
2023-09-19 19:55:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+document.getElementById("one-drum").addEventListener("click",clicked);
+function clicked(){
+    var audio = new Audio('tom-1.mp3');
+    audio.play();
+}
+
+document.getElementById("2-drum").addEventListener("click",
+function(){
+    var audio = new Audio('tom-2.mp3');
+    audio.play();
+}
+)
+document.getElementById("3-drum").addEventListener("click",
+function(){
+    var audio = new Audio('tom-3.mp3');
+    audio.play();
+}
+)
+document.getElementById("4-drum").addEventListener("click",
+function(){
+    var audio = new Audio('tom-4.mp3');
+    audio.play();
+}
+)
+document.getElementById("5-drum").addEventListener("click",
+function(){
+    var audio = new Audio('snare.mp3');
+    audio.play();
+}
+)
+document.getElementById("6-drum").addEventListener("click",
+function(){
+    var audio = new Audio('crash.mp3');
+    audio.play();
+}
+)
+
+document.getElementById("7-drum").addEventListener("click",
+function(){
+    var audio = new Audio('kick-bass.mp3');
+    audio.play();
+}
+)
+
+
+
+
